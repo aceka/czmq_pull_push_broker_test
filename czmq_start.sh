01e443b044112d26bb7b87a3c0383af06a233e42
Ali Cihat Kelle <ackelle@hotmail.com>
2016-02-24 14:28:01 +0200

    Delete czmq_start.sh

--- czmq_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gnome-terminal -e "bash -ic \"./device; exec bash\"" &
-gnome-terminal -e "bash -ic \"./server; exec bash\"" &
-gnome-terminal -e "bash -ic \"./client1; exec bash\"" &
-gnome-terminal -e "bash -ic \"./client2; exec bash\"" &
-gnome-terminal -e "bash -ic \"./client3; exec bash\"" &
-gnome-terminal -e "bash -ic \"./client4; exec bash\""
